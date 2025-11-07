be3cc1b179d1ee34fe83f2c98851a849f14dcd7a
gacastillo2332-collab <gacastillo2332@gmail.com>
2025-11-07 09:13:51 -0500

    hol

--- Calculadora/obj/Debug/net8.0-windows/Calculadora.AssemblyInfo.cs
+++ Calculadora/obj/Debug/net8.0-windows/Calculadora.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Calculadora")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+29b7944d74e7a212902323bf95f4af980ccb065d")]
 [assembly: System.Reflection.AssemblyProductAttribute("Calculadora")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Calculadora")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
